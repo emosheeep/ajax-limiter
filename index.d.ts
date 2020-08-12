5e9367eef438ae038e75ddd8871d554548712819
秦旭洋 <qinxuyang@bytedance.com>
2020-08-12 10:46:15 +0800

    fix: 声明文件调整，兼容commonjs规范

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,15 @@
+export type FuncArrWithPromise = Array<() => Promise<any>>
+
+export interface Config {
+  limit?: number;
+  exitWhenError?: boolean;
+  success?: (data: any, index: number) => any;
+  error?: (e: Error, index: number) => any;
+}
+
+export default function limiter(arr: FuncArrWithPromise, config?: Config): Promise<any>
+
+// 兼容 commonjs 模块规范
 declare module 'ajax-limiter' {
-  export type FuncArrWithPromise = Array<() => Promise<any>>
-
-  export interface Config {
-    limit?: number;
-    exitWhenError?: boolean;
-    success?: (data: any, index: number) => any;
-    error?: (e: Error, index: number) => any;
-  }
-
-  export default function limiter(arr: FuncArrWithPromise, config?: Config): Promise<any>
+  export = limiter
 }
